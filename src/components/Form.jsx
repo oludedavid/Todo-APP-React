9900e76ea26d451faf682b8d8f8c0eaf3f6bccf9
Olude david <odavidolumide@gmail.com>
2023-04-16 00:32:43 +0200

    added the edit and update functions or event completing the CRUD app and tried to store in local storage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,19 +6,109 @@ import { v4 as uuidv4 } from "uuid";
 //form component
 
 export default function Form() {
-  const [text, setText] = useState("");
-  const [date, setDate] = useState("");
-  const [todos, setTodos] = useState([]);
+  /*********************** State ******************/
+
+  const [todos, setTodos] = React.useState([]);
+  const [formData, setFormData] = useState({
+    objectiveText: "",
+    objectiveDate: "",
+    id: "",
+  });
+
+  const [updateFormData, setUpdateFormData] = React.useState({
+    updateObjectiveText: "",
+    updateObjectiveDate: "",
+  });
+
   const [visible, setVisible] = useState(false);
+  const [popUp, setPopUp] = useState(false);
 
-  // function handleDelete(index) {
-  //   setTodos((prevTodo) => {
-  //     const newTodo = [...prevTodo];
-  //     newTodo.splice(index, 1);
-  //     return newTodo;
-  //   });
-  // }
+  /*********************** Event handlers ******************/
 
+  //handle change
+  //the onChange event handler gets the form data and store in the formData state
+  function handleChange(event) {
+    event.preventDefault();
+    const { name, value } = event.target;
+    //push the value of the input to the formData array state
+    setFormData((prevFormData) => {
+      return { ...prevFormData, [name]: value, id: uuidv4() };
+    });
+  }
+
+  //create a new item: push the formData to the todos array state
+  function handleCreateItem() {
+    //push the formData to the todos array state
+    setTodos((prevValue) => [
+      ...prevValue,
+      {
+        objectiveText: formData.objectiveText,
+        objectiveDate: formData.objectiveDate,
+        id: uuidv4(),
+      },
+    ]);
+
+    // make the todo list visible
+    setVisible(true);
+
+    // clear the input fields
+    setFormData({ objectiveText: "", objectiveDate: "" });
+  }
+
+  //edit an item
+  function handleEdit(id) {
+    // find the item to edit
+    const item = todos.find((todo) => todo.id === id);
+
+    // set the initial edit form data to the values of the item
+    setUpdateFormData({
+      updateObjectiveText: item.objectiveText,
+      updateObjectiveDate: item.objectiveDate,
+      updateId: id,
+    });
+
+    // open the pop-up window
+    setPopUp(true);
+  }
+
+  //update an item onChange event handler: get the updated value and store it in the updateFormData state
+  function handleUpdateChange(event) {
+    const { name, value } = event.target;
+    //set the value of the text and date input of the corresponding item with the same id with the updated value
+    setUpdateFormData((prevFormData) => {
+      //we are only updating the value of the input that has been changed and leaving the id unchanged
+      return { ...prevFormData, [name]: value };
+    });
+  }
+
+  //update an item
+  function handleUpdate() {
+    //find the item to update
+    const item = todos.find((todo) => todo.id === updateFormData.updateId);
+
+    //update the item
+    item.objectiveText = updateFormData.updateObjectiveText;
+    item.objectiveDate = updateFormData.updateObjectiveDate;
+
+    //update the state of the todos array with the updated item
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === item.id) {
+          return item;
+        } else {
+          return todo;
+        }
+      });
+    });
+
+    //close the popUp window
+    setPopUp(false);
+
+    //alert the user that the item has been updated
+    alert("The item has been updated");
+  }
+
+  //delete an item
   function handleDelete(id) {
     setTodos((prevTodo) => {
       const newTodo = prevTodo.filter((todo) => todo.id !== id);
@@ -26,58 +116,51 @@ export default function Form() {
     });
   }
 
-  function handleEdit(index, updatedTodo) {
-    setTodos((prevTodo) => {
-      const newTodo = [...prevTodo];
-      newTodo[index] = updatedTodo;
-      return newTodo;
-    });
+  //close the popUp window
+  function handleClose() {
+    setPopUp(false);
   }
 
-  //handle click event
-  function handleAddItem() {
-    setTodos((prevValue) => [
-      ...prevValue,
-      {
-        objectiveText: text,
-        objectiveDate: date,
-        id: uuidv4(),
-      },
-    ]);
-    setVisible(true);
-  }
+  //store todos in local storage
+  React.useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
 
-  //Handle text change
-  function handleTextChange(e) {
-    setText(e.target.value);
-  }
+  //update local storage
+  React.useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-  //handle date change
-  function handleDateChange(e) {
-    //on change of date, add the value of date to the date state
-    setDate(e.target.value);
-  }
+  /*********************** Return ******************/
 
+  // return the form component
   return (
     <section className="form">
       <div className="form-container">
         <div>
           <input
             type="text"
+            value={formData.objectiveText}
+            name="objectiveText"
             placeholder="your objective"
             className="form-text form-input"
-            onChange={handleTextChange}
+            onChange={handleChange}
           />
         </div>
         <div>
           <input
             type="date"
+            value={formData.objectiveDate}
+            name="objectiveDate"
             className="form-date form-input"
-            onChange={handleDateChange}
+            onChange={handleChange}
           />
         </div>
         <div>
-          <button className="form-input" onClick={handleAddItem}>
+          <button className="form-input" onClick={handleCreateItem}>
             Add to objectives
           </button>
         </div>
@@ -91,6 +174,10 @@ export default function Form() {
             todo={todos}
             handleDelete={handleDelete}
             handleEdit={handleEdit}
+            handleUpdate={handleUpdate}
+            handleUpdateChange={handleUpdateChange}
+            popUp={popUp}
+            handleClose={handleClose}
           />
         )}
       </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,35 +4,68 @@ import { useState } from "react";
 //todo component
 
 export default function ToDo(props) {
-  //handle delete
-  function handleDelete(id) {
-    props.handleDelete(id);
-  }
-
-  //handle edit
-  function handleEdit() {
-    console.log("edit");
-  }
-
   return (
     <section className="todo">
       {props.todo.map((todo, index) => {
         return (
-          <div key={props.id} className="todo-container">
+          <div key={todo.id} className="todo-container">
             <input type="checkbox" className="todo-checkbox" />
             <h2>{todo.objectiveText}</h2>
             <p>{todo.objectiveDate}</p>
             <div className="btn-container">
               <button
                 className="delete-btn btn"
-                onClick={() => handleDelete(todo.id)}
+                onClick={() => props.handleDelete(todo.id)}
               >
                 <img src="../images/delete.png" className="delete icon" />
               </button>
-              <button className="edit-btn btn" onClick={handleEdit}>
+              <button
+                className="edit-btn btn"
+                onClick={() => props.handleEdit(todo.id)}
+              >
                 <img src="../images/edit.png" className="edit icon" />
               </button>
             </div>
+
+            {/**popUp window */}
+            {props.popUp && (
+              <div className="popUp-container">
+                <div className="popUp">
+                  <div className="popUp-form-container">
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="your objective"
+                        className="popUp-form-text popUp-form-input"
+                        name="updateObjectiveText"
+                        onChange={props.handleUpdateChange}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="date"
+                        className="popUp-form-date popUp-form-input"
+                        name="updateObjectiveDate"
+                        onChange={props.handleUpdateChange}
+                      />
+                    </div>
+
+                    <button
+                      className="popUp-form-input update-btn"
+                      onClick={props.handleUpdate}
+                    >
+                      Update objective
+                    </button>
+                  </div>
+                  <button
+                    className="popUp-form-input update-btn close-btn"
+                    onClick={props.handleClose}
+                  >
+                    Close window
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         );
       })}
